17a0ea85d6462c8c893a54d75d7970458f55ce52
Gábor Vass <90134776+Vassgabi89@users.noreply.github.com>
2021-11-12 19:03:00 +0100

    done

--- solutions/app2.js
+++ solutions/app2.js
@@ -1,7 +1,41 @@
+/*
+## 2. feladat
+- Fájl: `solutions/app2.js`
+- Objektum neve: `HU`
+- Export: a HU objektum legyen a default export!
+- Készíts egy `HU` nevű objektumot, amelynek az alábbi property-jei vannak:
+- `date()`: Visszaadja a paraméterként megadott dátumot a magyar dátumírás szabályainak megfelelően.
+- `currency()`: Visszaadja a paraméterként megadott számot a magyar pénzformátumnak megfelelően a Ft végződéssel együtt.
+- `list()`: A paraméterként kapott string tömböt visszaadja az alábbi formában: "első, második és harmadik" (tehát vesszővel elválasztva, az utolsó elem előtt az "és" szóval).*/
 
+let HU = {
+    date(date) {return new Intl.DateTimeFormat('hu-HU').format(date)},
+    curreny(currency) {return new Intl.NumberFormat('hu-HU', {
+        style: 'currency',
+        currency: 'HUF'
+    }).format(currency)},
+    list(array=[]) {return new Intl.ListFormat('hu-HU', {
+        style: 'long',
+        type: 'conjunction'
+    }).format(array)
+        /*
+        list = ''
+        for (i=0; i < array.length; i++) {
+            if (i === array.length-1) {list += ` és ${array[i]}`}
+                else {
+                    if (i === array.length-2) {list += `${array[i]}`}
+                        else {list += `${array[i]}, `}
+                }
+                return list*/
+    }
+}
 
-
-
-
-
-// export default HU
+/*
+date = new Date (2021,12,12)
+console.log(HU.date(date))
+console.log(HU.curreny(5400300,5))
+array = ['Miki', 'Maki', 'Menő', 'Manó']
+console.log(HU.list(array))
+*/
+ 
+export default HU
